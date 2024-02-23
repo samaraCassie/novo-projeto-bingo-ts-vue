611fed29fb0007f0ee98fe9ad87952afa499f431
samaraCassie <samaracassie@outlook.com>
2024-02-22 23:37:05 -0300

    feat: generating dynamically "CartelaBingo""

--- src/App.vue
+++ src/App.vue
@@ -2,29 +2,14 @@
   <v-app class="v-theme--dark roboto">
     <v-main>
       <TelaInicio />
-      <BoxDadosRodada :nomeJogador="nomeJogador" v-if="post"></BoxDadosRodada>/>
-      <!-- <HelloWorld /> -->
-      <CartelaBingo />
+      <BoxDadosRodada></BoxDadosRodada>
+      <CartelaBingo></CartelaBingo>
     </v-main>
   </v-app>
 </template>
 
-<script>
-/* import HelloWorld from "./components/HelloWorld.vue"; */
+<script setup>
 import CartelaBingo from "./components/CartelaBingo.vue";
 import TelaInicio from "./components/TelaInicio.vue";
 import BoxDadosRodada from "./components/BoxDadosRodada.vue";
-
-export default {
-  name: "App",
-
-  components: {
-    /*HelloWorld, */
-    CartelaBingo,
-    TelaInicio,
-    BoxDadosRodada,
-  },
-
-  data() {},
-};
 </script>
